d92845a8b8c8d1f53a8630bfd130e57721b13457
Sebas5158R <sebastianriveraaviles4@gmail.com>
2023-08-11 09:05:22 -0500

    Third tables completes

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/flask_shopy_2687386'
+    SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@localhost/flask_shopy_2687386'
     SQLALCHEMY_TRACK_MODIFICATION=False
     
