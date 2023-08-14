24c43b125c4473f2c56d470608ebcd4dcb123c52
Sebas5158R <sebastianriveraaviles4@gmail.com>
2023-08-14 10:30:10 -0500

    Formulario cliente

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 #importaciones del proyecto
-from flask import Flask
+from flask import Flask, render_template
 from config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
 
 
 #inicializar el objeto flask
@@ -14,6 +16,13 @@ app.config.from_object(Config)
 #iniciacializar a continuacion el objeto SQLalchemy
 db=SQLAlchemy(app)
 Migrate(app , db)
+
+# Crear formulario de registro de clientes
+class RegistroClienteForm(FlaskForm):
+    user_name = StringField("Nombre de Usuario")
+    email = StringField("Email")
+    password = PasswordField("Password")
+    submit = SubmitField("Registrar")
 
 
 #modelos - entidades del proyecto
@@ -37,3 +46,32 @@ class Ventas(db.Model):
     id= db.Column(db.Integer,primary_key=True)
     fecha= db.Column(db.DateTime, default = datetime.utcnow)
     cliente_id=db.Column(db.Integer, db.ForeignKey('clientes.id'))
+
+class Detalles(db.Model):
+    __tablename__= "detalles"
+    id= db.Column(db.Integer,primary_key=True)
+    producto_id = db.Column(db.Integer, db.ForeignKey('productos.id'))
+    venta_id=db.Column(db.Integer, db.ForeignKey('ventas.id'))
+    cantidad = db.Column(db.Integer)
+
+
+@app.route('/clientes/create', 
+           methods = ['GET', 'POST'])
+def crear_cliente():
+    # instanciar formulario
+    form = RegistroClienteForm()
+    if form.validate_on_submit():
+        # crear el nuevo cliente
+        c = Cliente(user_name = form.user_name.data, email = form.email.data, password = form.password.data)
+        db.session.add(c)
+        db.session.commit()
+        return "Cliente registrado con éxito"
+    return render_template('registro.html', form = form)
+
+
+@app.route('/clientes',
+           methods = ['GET'])
+def listar_clientes():
+    #seleccionar todos los clientes
+    clientes = Cliente.query.all()
+    return render_template('listar_clientes.html', clientes=clientes)
--- config.py
+++ config.py
@@ -2,3 +2,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:1234@localhost/flask_shopy_2687386'
     SQLALCHEMY_TRACK_MODIFICATION=False
     
+    # Para encriptar las passwords
+    SECRET_KEY = 'FICHA2687386HOLASBROSKISLABUENAPATODOS'
+    
